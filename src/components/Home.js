4f5bb23d02c74f6b3b7a075c9e9d0a824bd01f6a
Daniel <danielbahia12@yahoo.com.br>
2022-07-07 18:30:21 -0300

    Requisito 05 - Ok

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { getProductsFromCategoryAndQuery } from '../services/api';
+import Card from './Card';
 
 class Home extends React.Component {
   constructor() {
     super();
     this.state = {
       inputSearch: '',
+      productList: [],
     };
   }
 
-  render() {
+  componentDidMount() {
+    console.log(getProductsFromCategoryAndQuery('computador'));
+  }
+
+  searchProducts = async () => {
     const { inputSearch } = this.state;
+    const productsList = await getProductsFromCategoryAndQuery('', inputSearch);
+    this.setState({ productList: productsList.results });
+  }
+
+  handleChange = (event) => {
+    this.setState({ inputSearch: event.target.value });
+  };
+
+  render() {
+    const { inputSearch, productList } = this.state;
     return (
       <>
         <input
@@ -23,7 +39,7 @@ class Home extends React.Component {
         <button
           type="button"
           data-testid="query-button"
-          onClick={ console.log('olá') }
+          onClick={ this.searchProducts }
         >
           Pesquisar
         </button>
@@ -34,6 +50,11 @@ class Home extends React.Component {
           Digite algum termo de pesquisa ou escolha uma categoria.
 
         </h2>
+
+        { productList.length === 0
+          ? <p> Nenhum produto foi encontrado </p>
+          : <Card results={ productList } />}
+
       </>
 
     );
