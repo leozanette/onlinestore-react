bc4735305d94a279a4a42b937b6716545a298194
Daniel <danielbahia12@yahoo.com.br>
2022-07-08 20:08:36 -0300

    Requisito 09 - Ok

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { getProductDetail } from '../services/api';
 
 class ProductDetail extends React.Component {
@@ -21,6 +22,12 @@ renderProductDetail = async () => {
   this.setState({ result });
 }
 
+passData = () => {
+  const { getFromHome } = this.props;
+  const { result } = this.state;
+  getFromHome([result]);
+}
+
 render() {
   const { result } = this.state;
   return (
@@ -28,6 +35,18 @@ render() {
       <img src={ result.thumbnail } alt={ result.title } />
       <p data-testid="product-detail-name">{result.title}</p>
       <p>{result.price }</p>
+      <input
+        data-testid="product-detail-add-to-cart"
+        type="button"
+        onClick={ this.passData }
+        id={ result.id }
+      />
+      <Link
+        data-testid="shopping-cart-button"
+        to="/cart"
+      >
+        Cart
+      </Link>
     </div>
   );
 }
@@ -35,6 +54,7 @@ render() {
 
 ProductDetail.propTypes = {
   location: PropTypes.objectOf(PropTypes.string).isRequired,
+  getFromHome: PropTypes.func.isRequired,
 };
 
 export default ProductDetail;
