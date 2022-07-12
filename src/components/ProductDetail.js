2e96407433bcbb49bd719cbb8a285711d17f6706
Daniel <danielbahia12@yahoo.com.br>
2022-07-12 15:02:09 -0300

    Requisito 11 - em Desenvolvimento

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -11,6 +11,7 @@ class ProductDetail extends React.Component {
       rating: '',
       ratingEmail: '',
       ratingText: '',
+      avaliation: [],
     };
   }
 
@@ -26,7 +27,6 @@ renderProductDetail = async () => {
   this.setState({ result });
 }
 
-
 handleChange = (e) => {
   const { target } = e;
   const { value } = target;
@@ -34,141 +34,167 @@ handleChange = (e) => {
   this.setState({ [name]: value });
 }
 
-handleSaveButton = () => {
+handleSaveButton = (e) => {
+  e.preventDefault();
+  localStorage.clear();
   const { rating, ratingEmail, ratingText } = this.state;
-  localStorage
-    .setItem('rating', rating);
-  localStorage
-    .setItem('ratingEmail', ratingEmail);
-  localStorage
-    .setItem('ratingText', ratingText);
+  const avaliacao = {
+    ratingEmail,
+    rating,
+    ratingText,
+  };
+
+  this.setState((prevState) => ({
+    avaliation: [...prevState.avaliation, avaliacao],
+    ratingEmail: '',
+    ratingText: '',
+  }), () => this.saveLocalStorage());
 }
 
 getLocalStorage = () => {
-  // const { rating, ratingEmail, ratingText } = this.state;
-  const ratingStorage = localStorage.getItem('rating');
-  const emailStorage = localStorage.getItem('ratingEmail');
-  const textStorage = localStorage.getItem('ratingText');
+  const testeLocalStorage = localStorage.getItem('avaliacoes');
 
-  this.setState({ rating: ratingStorage });
-  this.setState({ ratingEmail: emailStorage });
-  this.setState({ ratingText: textStorage });
+  const parse = JSON.parse(testeLocalStorage);
 
-passData = () => {
-  const { getFromHome } = this.props;
-  const { result } = this.state;
-  getFromHome([result]);
+  if (testeLocalStorage !== null) {
+    this.setState({ avaliation: parse });
+  }
 }
 
-render() {
-  const { result, rating, ratingEmail, ratingText } = this.state;
-  return (
-    <>
-      <div>
-        <img src={ result.thumbnail } alt={ result.title } />
-        <p data-testid="product-detail-name">{result.title}</p>
-        <p>{result.price }</p>
-      <input
-        data-testid="product-detail-add-to-cart"
-        type="button"
-        onClick={ this.passData }
-        id={ result.id }
-      />
-      <Link
-        data-testid="shopping-cart-button"
-        to="/cart"
-      >
-        Cart
-      </Link>
-      </div>
-      <div>
-        <form>
-          <input
-            data-testid="product-detail-email"
-            type="email"
-            placeholder="email"
-            name="ratingEmail"
-            onChange={ this.handleChange }
-          />
-          <input
-            type="textarea"
-            data-testid="product-detail-evaluation"
-            placeholder="Escreva sua avaliação"
-            maxLength="2000"
-            name="ratingText"
-            onChange={ this.handleChange }
-          />
-          <label htmlFor="rating">
-            <input
-              type="radio"
-              value="1"
-              data-testid="1-rating"
-              name="rating"
-              onChange={ this.handleChange }
-            />
-            1
-          </label>
-          <label htmlFor="rating">
-            <input
-              type="radio"
-              value="2"
-              data-testid="2-rating"
-              name="rating"
-              onChange={ this.handleChange }
-            />
-            2
-          </label>
-          <label htmlFor="rating">
-            <input
-              type="radio"
-              value="3"
-              data-testid="3-rating"
-              name="rating"
-              onChange={ this.handleChange }
-            />
-            3
-          </label>
-          <label htmlFor="rating">
-            <input
-              type="radio"
-              value="4"
-              data-testid="4-rating"
-              name="rating"
-              onChange={ this.handleChange }
-            />
-            4
-          </label>
-          <label htmlFor="rating">
-            <input
-              type="radio"
-              value="5"
-              data-testid="5-rating"
-              name="rating"
-              onChange={ this.handleChange }
-            />
-            5
-          </label>
-          <br />
+saveLocalStorage = () => {
+  const teste = JSON.stringify(this.state.avaliation);
+  localStorage.setItem('avaliacoes', teste);
+}
+
+  passData = () => {
+    const { getFromHome } = this.props;
+    const { result } = this.state;
+    getFromHome([result]);
+  };
+
+  render() {
+    const { result, avaliation } = this.state;
+    return (
+      <>
+        <div>
+          <img src={ result.thumbnail } alt={ result.title } />
+          <p data-testid="product-detail-name">{result.title}</p>
+          <p>{result.price }</p>
           <button
-            type="submit"
-            data-testid="submit-review-btn"
-            onClick={ this.handleSaveButton }
+            data-testid="product-detail-add-to-cart"
+            type="button"
+            onClick={ this.passData }
+            id={ result.id }
           >
-            Enviar avaliação
+            Add to Cart
           </button>
-        </form>
-      </div>
-      <div>
-        <p>{ rating }</p>
-        <br />
-        <p>{ ratingEmail }</p>
-        <br />
-        <p>{ ratingText }</p>
-        <br />
-      </div>
-    </>
-  );
-}
+
+          <Link
+            data-testid="shopping-cart-button"
+            to="/cart"
+          >
+            Cart
+          </Link>
+        </div>
+        <div>
+          <h2> Avaliações </h2>
+          <form>
+            <input
+              data-testid="product-detail-email"
+              type="email"
+              placeholder="email"
+              name="ratingEmail"
+              onChange={ this.handleChange }
+              value={ this.state.ratingEmail }
+            />
+            <label htmlFor="rating">
+              <input
+                type="radio"
+                value="1"
+                data-testid="1-rating"
+                name="rating"
+                onChange={ this.handleChange }
+              />
+              1
+            </label>
+            <label htmlFor="rating">
+              <input
+                type="radio"
+                value="2"
+                data-testid="2-rating"
+                name="rating"
+                onChange={ this.handleChange }
+              />
+              2
+            </label>
+            <label htmlFor="rating">
+              <input
+                type="radio"
+                value="3"
+                data-testid="3-rating"
+                name="rating"
+                onChange={ this.handleChange }
+              />
+              3
+            </label>
+            <label htmlFor="rating">
+              <input
+                type="radio"
+                value="4"
+                data-testid="4-rating"
+                name="rating"
+                onChange={ this.handleChange }
+              />
+              4
+            </label>
+            <label htmlFor="rating">
+              <input
+                type="radio"
+                value="5"
+                data-testid="5-rating"
+                name="rating"
+                onChange={ this.handleChange }
+              />
+              5
+            </label>
+            <input
+              type="textarea"
+              data-testid="product-detail-evaluation"
+              placeholder="Escreva sua avaliação"
+              maxLength="2000"
+              name="ratingText"
+              onChange={ this.handleChange }
+              value={ this.state.ratingText }
+            />
+            <button
+              type="submit"
+              data-testid="submit-review-btn"
+              onClick={ this.handleSaveButton }
+            >
+              Enviar avaliação
+            </button>
+          </form>
+          <div>
+            { avaliation.map((item, index) => (
+              <div key={ index }>
+                <p>{ item.rating }</p>
+                <p>{ item.ratingEmail }</p>
+                <p>{ item.ratingText }</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* <div>
+          <p>{ rating }</p>
+          <br />
+          <p>{ ratingEmail }</p>
+          <br />
+          <p>{ ratingText }</p>
+          <br />
+        </div> */}
+      </>
+    );
+  }
 }
 
 ProductDetail.propTypes = {
