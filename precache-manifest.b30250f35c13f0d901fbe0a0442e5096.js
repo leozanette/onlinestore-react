edb7173e4e5f22662fd7b9d165a1f20629279f74
Leonardo Zanette <leonardobzanette@gmail.com>
2023-01-13 10:30:57 -0300

    Updates

---
+++ precache-manifest.b30250f35c13f0d901fbe0a0442e5096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae4c05530bb556bea0d57cbfe9886a5",
+    "url": "/onlinestore/index.html"
+  },
+  {
+    "revision": "6d65e7d961a97168cf89",
+    "url": "/onlinestore/static/css/main.a1d8f850.chunk.css"
+  },
+  {
+    "revision": "4e412237d0abe7871382",
+    "url": "/onlinestore/static/js/2.6a9f1312.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/onlinestore/static/js/2.6a9f1312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d65e7d961a97168cf89",
+    "url": "/onlinestore/static/js/main.de395f6c.chunk.js"
+  },
+  {
+    "revision": "1d90905f1843ddf0a815",
+    "url": "/onlinestore/static/js/runtime-main.f0d86cfb.js"
+  }
+]);
